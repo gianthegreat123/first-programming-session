a42638817f032183c626321ed89f81a6bae42dcb
gianthegreat123 <gdg1835@students.uc-bcf.edu.ph>
2024-09-06 08:48:44 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ *
+ * @author m303user
+ //Anna
+ //September 6,2024
+ //First Programing session*/
+public class Test {
+    public static void main(String[]args){
+        System.out.print("Ma'am \t\t Rhodora\nQuitaleg");
+        //Java Escape characters
+        System.out.println("\n\n\nAnna\"s Class");
+        System.out.print("I Love Java Programing");
+    }
+}
